693723c68ddeffbfa53c20c6a3accd1a62b7fa1e
kalharawimukthi0617 <kalhara19970617@gmail.com>
2021-05-19 07:44:23 +0530

    creating classes for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,67 @@
-
-
-        @extends('layouts.auth-front-end')
+@extends('layouts.auth-front-end')
         @section('content')
-
         <center>
-            <form style="margin-top: 300px" method="POST" action="{{ route('login') }}">
-                @csrf
-    
-                <!-- Email Address -->
-                <div>
-                    <x-label for="email" :value="__('Email')" />
-    
-                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
+            <div class="container">
+
+                <div class="loginCard">
+
+
+                    <div class="col-md-12">
+
+                        <div class="formStyle">
+                
+                             
+                            <form style="margin-top: 300px" method="POST" action="{{ route('login') }}">
+                                @csrf
+                    
+                                <!-- Email Address -->
+                                <div>
+                                    <x-label for="email" :value="__('Email')" />
+                    
+                                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                                </div>
+                    
+                                <!-- Password -->
+                                <div class="mt-4">
+                                    <x-label for="password" :value="__('Password')" />
+                    
+                                    <x-input id="password" class="block mt-1 w-full"
+                                                    type="password"
+                                                    name="password"
+                                                    required autocomplete="current-password" />
+                                </div>
+                    
+                                <!-- Remember Me -->
+                                <div class="block mt-4">
+                                    <label for="remember_me" class="inline-flex items-center">
+                                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                    </label>
+                                </div>
+                    
+                                <div class="flex items-center justify-end mt-4">
+                                    @if (Route::has('password.request'))
+                                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                            {{ __('Forgot your password?') }}
+                                        </a>
+                                    @endif
+                    
+                                    <x-button class="ml-3">
+                                        {{ __('Log in') }}
+                                    </x-button>
+                                </div>
+                            </form>
+                
+                           
+                        </div>
+
+                    </div>
+                   
                 </div>
-    
-                <!-- Password -->
-                <div class="mt-4">
-                    <x-label for="password" :value="__('Password')" />
-    
-                    <x-input id="password" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password"
-                                    required autocomplete="current-password" />
-                </div>
-    
-                <!-- Remember Me -->
-                <div class="block mt-4">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                    </label>
-                </div>
-    
-                <div class="flex items-center justify-end mt-4">
-                    @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </a>
-                    @endif
-    
-                    <x-button class="ml-3">
-                        {{ __('Log in') }}
-                    </x-button>
-                </div>
-            </form>
+
+            </div>
+            
         </center>
         
         @endsection
